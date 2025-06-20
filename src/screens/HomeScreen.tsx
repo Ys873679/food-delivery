75b3ff21468b5f81e42191bdd914bc3f34fd1bda
ys873679 <ys873679@gmail.com>
2025-06-20 18:04:01 +0530

    homebanner created

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,13 +1,27 @@
-import { IonContent,  IonPage,} from '@ionic/react';
+import { IonContent,IonPage } from '@ionic/react';
+
 import './HomeScreen.scss';
+import FilterComponent from '../components/homeComponent/FilterComponent';
+import HomeBanner from '../components/homeComponent/HomeBanner';
+
+
 
 const HomeScreen: React.FC = () => {
   return (
     <IonPage>
       
       <IonContent fullscreen>
-       
-         
+        <div className='home_screen'>
+          <div className='header_section'>
+            <FilterComponent/>
+          </div>
+          <div className='body_section'>
+            <div className="banner_section">
+              <HomeBanner/>
+            </div>
+           
+          </div>
+        </div>
         
       </IonContent>
     </IonPage>
